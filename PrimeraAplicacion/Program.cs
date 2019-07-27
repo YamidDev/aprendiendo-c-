e9c7f67af2f083cf4a623ccebd42d191ac7c09e0
Yamid <yamidcuetomazo@gmail.com>
2019-07-27 10:58:29 -0500

    Agregar archivos de proyecto.

---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PrimeraAplicacion{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Bienvenidos al curso de C#");
+        }
+    }
+}
